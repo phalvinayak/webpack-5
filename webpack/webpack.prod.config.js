f0f4b15a964cf6054cf49e3e5c39da5266b04586
Vinayak Phal <phalvinayak@gmail.com>
2023-03-26 18:03:17 +0530

    Added css minimizer plugin for css minification

--- webpack/webpack.prod.config.js
+++ webpack/webpack.prod.config.js
@@ -1,12 +1,31 @@
 const commonConfig = require("./webpack.common.config");
 const { merge } = require("webpack-merge");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = merge(commonConfig, {
   output: {
     filename: "js/[name].[contenthash:12].js",
   },
   mode: "production",
+  optimization: {
+    minimize: true,
+    minimizer: [
+      `...`,
+      new CssMinimizerPlugin({
+        minimizerOptions: {
+          preset: [
+            "default",
+            {
+              discardComments: {
+                removeAll: true,
+              },
+            },
+          ],
+        },
+      }),
+    ],
+  },
   module: {
     rules: [
       {
